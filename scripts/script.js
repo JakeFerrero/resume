c28612ba8d4f1bfd4702c64075a87ecc106b8d50
Jake Ferrero <jake.a.ferrero@gmail.com>
2018-08-20 02:00:31 -0400

    Need to add picture, but other than that, complete

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,24 @@
-function move(val) {
-    var elem = document.getElementById("bar");
+function javabar(val) {
+    var bar = document.getElementById("java-progress");
     var width = 1;
-    var id = setInterval(frame, 8);
+    var animation = setInterval(frame, 8);
     function frame() {
-        if (width >= val*10) {
-            clearInterval(id);
+        if (width >= val * 10) {
+            clearInterval(animation);
+        } else {
+            width++;
+            bar.style.width = width + '%';
+        }
+    }
+}
+
+function jsbar(val) {
+    var elem = document.getElementById("js-progress");
+    var width = 1;
+    var animation = setInterval(frame, 8);
+    function frame() {
+        if (width >= val * 10) {
+            clearInterval(animation);
         } else {
             width++;
             elem.style.width = width + '%';
@@ -12,4 +26,110 @@ function move(val) {
     }
 }
 
-window.onload = move(8);
+function unixbar(val) {
+    var elem = document.getElementById("unix-progress");
+    var width = 1;
+    var animation = setInterval(frame, 8);
+    function frame() {
+        if (width >= val * 10) {
+            clearInterval(animation);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+}
+
+function cbar(val) {
+    var elem = document.getElementById("c-progress");
+    var width = 1;
+    var animation = setInterval(frame, 8);
+    function frame() {
+        if (width >= val * 10) {
+            clearInterval(animation);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+}
+
+function pythonbar(val) {
+    var elem = document.getElementById("python-progress");
+    var width = 1;
+    var animation = setInterval(frame, 8);
+    function frame() {
+        if (width >= val * 10) {
+            clearInterval(animation);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+}
+
+function gobar(val) {
+    var elem = document.getElementById("go-progress");
+    var width = 1;
+    var animation = setInterval(frame, 8);
+    function frame() {
+        if (width >= val * 10) {
+            clearInterval(animation);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+}
+
+function linuxbar(val) {
+    var elem = document.getElementById("linux-progress");
+    var width = 1;
+    var animation = setInterval(frame, 8);
+    function frame() {
+        if (width >= val * 10) {
+            clearInterval(animation);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+}
+
+function macbar(val) {
+    var elem = document.getElementById("mac-progress");
+    var width = 1;
+    var animation = setInterval(frame, 8);
+    function frame() {
+        if (width >= val * 10) {
+            clearInterval(animation);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+}
+
+function windowsbar(val) {
+    var elem = document.getElementById("windows-progress");
+    var width = 1;
+    var animation = setInterval(frame, 8);
+    function frame() {
+        if (width >= val * 10) {
+            clearInterval(animation);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+}
+
+window.onload = javabar(10);
+window.onload = jsbar(8);
+window.onload = unixbar(9);
+window.onload = cbar(8);
+window.onload = pythonbar(7);
+window.onload = gobar(7);
+window.onload = linuxbar(10);
+window.onload = macbar(10);
+window.onload = windowsbar(8);
